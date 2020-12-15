47bf65ffe233c1d38bf50842c1ed5ee8d883e172
TunyakarnK <tunyakarn2544@gmail.com>
2020-12-15 17:24:48 +0700

    finished

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int n=-1;
+	int x=0;
+	int y=0;
+	while (n !=0){
+     	cout << "Enter an integer: ";
+     	cin >> n;
+     	
+ 		if(n==0){
+ 			break;
+		 }
+ 		else if(n%2!=0){	
+ 			y++;}
+		else {
+	 		x++;
+		 }
+	 }
+	 	
+    cout << "#Even numbers = " << x <<"\n";
+    cout << "#Odd numbers = " << y ;
     return 0;
 }
