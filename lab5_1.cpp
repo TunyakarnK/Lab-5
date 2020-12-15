9abd67f0635fe8f2173d20e7f56551a1d48bc8db
TunyakarnK <tunyakarn2544@gmail.com>
2020-12-15 17:56:25 +0700

    finished

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,25 +1,56 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
+double deg2rad(double d){
+    double R;
+	R = d*M_PI/180;
+    return R;
+}
+double rad2deg(double r){
+	double D ;
+	D= r*180/M_PI;
+	return D;
+}
+double findXComponent(double c,double d,double e,double f){
+	double xComp;
+	xComp= (c*cos(e))+(d*cos(f));
+	return xComp;
+}
+double findYComponent(double g,double h,double i,double j){
+	double yComp;
+	yComp= (g*sin(i))+(h*sin(j));
+	return yComp;
+}
+double pythagoras (double x,double y){
+	double p; 
+	p= sqrt(pow(x,2)+pow(y,2));
+	return p;
+}
+void showResult(double a,double b){
+	cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+	cout << "Length of the resultant vector = " << a;
+	cout << "\nDirection of the resultant vector (deg) = " << b;
+	cout << "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" ;
+}
+
 
 int main(){
-    int n=-1;
-	int x=0;
-	int y=0;
-	while (n !=0){
-     	cout << "Enter an integer: ";
-     	cin >> n;
-     	
- 		if(n==0){
- 			break;
-		 }
- 		else if(n%2!=0){	
- 			y++;}
-		else {
-	 		x++;
-		 }
-	 }
-	 	
-    cout << "#Even numbers = " << x <<"\n";
-    cout << "#Odd numbers = " << y ;
-    return 0;
+    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
+    cout << "Enter length of the first vector: ";
+    cin >> l1;
+    cout << "Enter direction of the first vector (deg): ";
+    cin >> a1;
+    cout << "Enter length of the second vector: ";
+    cin >> l2;
+    cout << "Enter direction of the second vector (deg): ";
+    cin >> a2;
+    
+    a1 = deg2rad(a1);
+    a2 = deg2rad(a2);
+    xcomp = findXComponent(l1,l2,a1,a2);
+    ycomp = findYComponent(l1,l2,a1,a2);
+    result_vec_length = pythagoras(xcomp,ycomp);
+    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
+
+    showResult(result_vec_length,result_vec_direction);
 }
